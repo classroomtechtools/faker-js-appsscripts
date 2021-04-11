999c97dd8558f84f9ff3dadc389a2183b4136108
Adam Morris <amorris@mistermorris.com>
2021-04-11 19:00:02 +0800

    initial

--- src/scripts/Export.js
+++ src/scripts/Export.js
@@ -2,7 +2,7 @@
  * Functions defined here, or any written in the parent directory
  * are put into the project as regular appscripts files (next to appsscript.json)
  */
-function myFunction () {
-    const {anything} = Import;  // or const anything = Import.anything;
-    // do anything
+function module () {
+    const {Faker} = Import;  // or const anything = Import.anything;
+    return Faker;
 }
